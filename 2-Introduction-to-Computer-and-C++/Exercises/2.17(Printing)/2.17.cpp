23c37884fc9d7539c263779af25d6c17d8a8dd4f
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-03-03 21:33:03 -0800

    new exercises

---
+++ 2-Introduction-to-Computer-and-C++/Exercises/2.17(Printing)/2.17.cpp
@@ -0,0 +1,5 @@
+// Write a program that prints the numbers 1 to 4 on the same line with each pair of adjacent numbers separated by one space. Do this several ways:
+//a) Using one statement with one stream insertion operator. 
+//b) Using one statement with four stream insertion operators. 
+//c) Using four statements.
+
---
+++ 2-Introduction-to-Computer-and-C++/Exercises/2.21(Display Shapes with Asterisks)/2.21.cpp
@@ -0,0 +1,16 @@
+// Write a program that prints a box, an oval, an arrow and a diamond as follows:
+#include <iostream>
+
+int main() {
+    std::cout << "********     ***       *       *\n";
+    std::cout << "*      *    *   *     ***     * *\n";
+    std::cout << "*      *   *     *   *****   *   *\n";
+    std::cout << "*      *   *     *     *    *     *\n";
+    std::cout << "*      *   *     *     *   *       *\n";
+    std::cout << "*      *   *     *     *    *     *\n";
+    std::cout << "*      *   *     *     *     *   *\n";
+    std::cout << "*      *    *   *      *      * *\n";
+    std::cout << "********     ***       *       *\n";
+    
+    return 0;
+}
