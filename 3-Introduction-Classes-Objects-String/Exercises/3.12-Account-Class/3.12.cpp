38da2225693d53bb0463cf3f9f298b38115592fd
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-03-04 22:45:00 -0800

    new exercise

---
+++ 3-Introduction-Classes-Objects-String/Exercises/3.12-Account-Class/3.12.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account 
+{
+  public:
+  explicit Account (int balance)
+  {
+     if (balance > 0)
+            accountBalance = balance;
+        else 
+        {
+            cerr << "Your initial balance is invalid. Setting balance to 0." << endl;
+            accountBalance = 0;
+        }
+  }
+
+  void setAccountBalance(int balance)
+  {
+    if (balance > 0)            
+      accountBalance = balance;
+    else 
+    {
+      cerr << "Invalid balance value. Balance remains: " << accountBalance << endl;
+    }    
+  }
+
+  int getAccountBalance() const
+  {
+    return accountBalance;
+  }
+  void displayAccountBalance() const
+  {
+    cout << getAccountBalance() << "!" << endl;
+  }
+
+  private:
+  int accountBalance;
+};
+
+int main()
+{
+  Account myAccount1(10);
+
+  cout << "Your actual balance is: " << myAccount1.getAccountBalance() << "!" << endl;
+
+  myAccount1.setAccountBalance(0);
+
+  cout << "Your new balance is: ";
+  myAccount1.displayAccountBalance();
+  
+};
