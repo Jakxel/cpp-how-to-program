50f7dc727a299fbcd7024f5e9e0c7a30114f1bed
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2025-07-01 22:34:29 -0500

    exercise done

--- 6-Functions-&-Introduction-to-Recursion/Exercices/611.cpp
+++ 6-Functions-&-Introduction-to-Recursion/Exercices/611.cpp
@@ -1,12 +1,24 @@
 #include <cmath>
 #include <iostream>
+#include <ostream>
 using namespace std;
 
 int main (){
   double x =0;
 
   x = fabs(7.5);
-  cout << x ;
-
+  cout << x << endl;
+  x = floor(7.5);
+  cout << x << endl;
+  x = fabs(0.0);
+  cout << x << endl;
+  x = ceill(0.0);
+  cout << x << endl;
+  x = fabs(-6.4);
+  cout << x << endl;
+  x = ceil(-6.5);
+  cout << x << endl;
+  x = ceil(-fabs( -8 + floor(-5.5)));
+  cout << x << endl;
   return 0;
 }
